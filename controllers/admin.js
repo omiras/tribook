1fd1500724093408ca16885b92fb7ebca16313ae
omiras <oscar.miras@gmail.com>
2024-09-02 19:43:22 +0200

    almost done edit apartment

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,18 +1,42 @@
 // importar el modelo
 const Apartment = require('../models/apartment.model.js');
 
+
 const getNewApartmentForm = (req, res) => {
+    res.render('new-apartment', {
+        apartment: {}
+    })
+}
 
-    // Obtener todos los apartmentos de la base de datos
-    const apartments = Apartment.find();
+const getEditAparmentForm = async (req, res) => {
+    // 1. Recuperar el apartmento identificado por su idApartment de la base de datos
+    const { idApartment } = req.params;
 
-    res.render('new-apartment.ejs')
+    // 2. Ir a la base de datos y obtener el apartamento dada su id
+    const apartment = await Apartment.findById(idApartment);
+
+    // 3. Pasar este apartmento a la vista para pre rellenar todos los campos
+    res.render('new-apartment', {
+        apartment
+    })
 }
 
 const postNewApartment = async (req, res) => {
 
     // Me han metido más servicios en el req.services que los servicios que yo quiero? kitchen, wifi, etc. res.status(400).send('Ha ocurrido un error');
-    
+
+    // ¿Cómo detecto si estoy añadiendo un apartamento o editando un apartamento? Si lo estoy editando, ya tengo id
+    const { id } = req.body;
+
+    if (id) {
+        // TODO: Completar
+        // 1. Utlizar el método más adecuado para buscar un documento dado su id y actualizar sus campos a partir del req.body
+
+        res.send('Apartamento actualizado');
+        return;
+    }
+
+
     await Apartment.create({
         title: req.body.title,
         description: req.body.description,
@@ -27,6 +51,7 @@ const postNewApartment = async (req, res) => {
 // named exports (expotamos varios recursos, lo hacemos como un objeto)
 module.exports = {
     getNewApartmentForm,
-    postNewApartment
+    postNewApartment,
+    getEditAparmentForm
 }
 
--- routes/admin.js
+++ routes/admin.js
@@ -11,5 +11,6 @@ const adminControllers = require('../controllers/admin.js');
 
 router.get('/apartment/new-apartment', adminControllers.getNewApartmentForm);
 router.post('/apartment/new-apartment', adminControllers.postNewApartment);
+router.get('/apartment/:idApartment/edit', adminControllers.getEditAparmentForm)
 
 module.exports = router;
