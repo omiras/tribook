eb69e961db52d1d7d1c4ceb6bfd5451a122cdd0f
omiras <oscar.miras@gmail.com>
2024-09-03 21:10:45 +0200

    detailed view for admin role no longer needed

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,6 @@ const indexControllers = require('../controllers/index.js')
 
 router.get('/apartment/new-apartment', adminControllers.getNewApartmentForm);
 router.post('/apartment/new-apartment', adminControllers.postNewApartment);
-router.get('/apartment/:idApartment', indexControllers.getApartmentById);
 router.get('/apartment/:idApartment/edit', adminControllers.getEditAparmentForm)
 
 module.exports = router;
