c8a5f9cfc1232d1b8f728c40e195d9f32ad0eed1
omiras <oscar.miras@gmail.com>
2024-09-03 21:14:08 +0200

    twice defined same endpoint

--- app.js
+++ app.js
@@ -32,11 +32,6 @@ app.use(session({
 }));
 
 
-// endpoint para gestionar el logout
-app.get('/logout', (req, res) => {
-
-});
-
 // Vamos a pasar una variable a todas las vistas. Vamos a indicar si el usuario que está accediendo a la vista es de tipo administrador o no. Eventualmente, esta información debería proceder de una base de datos de usuario. Ahora mismo todas las rutas '/admin' se considera que accede un usuario de tipo adiministrador
 app.use((req, res, next) => {
     // La variable req.locals es una variable "global" de tipo objecto a la que todas las vistas pueden acceder
