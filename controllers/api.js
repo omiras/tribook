eb6cfa89123ecb251e83ab54ac64afd6938186b5
omiras <oscar.miras@gmail.com>
2024-09-19 16:36:20 +0200

    search my query

--- controllers/api.js
+++ controllers/api.js
@@ -6,7 +6,7 @@ const getApartments = async (req, res) => {
 
     if (limit < 1 || limit > 100000) {
         return res.status(400).json({
-            message: "Limit parameter must be between 1 and 100000"
+            message: "'Limit' parameter must be between 1 and 100000"
         })
     }
 
@@ -17,6 +17,28 @@ const getApartments = async (req, res) => {
     })
 }
 
+const searchApartments = async (req, res) => {
+    const { q } = req.query;
+
+    if (!q || q.length < 3 || q.length > 100) {
+        return res.status(400).json({
+            message: "'q' parameter must be between 3 and 100 characters long"
+        })
+    }
+
+    const apartments = await Apartment.find({
+        $or: [
+            { title: new RegExp(q, 'i') },
+            { description: new RegExp(q, 'i') }
+        ]
+    });
+    return res.status(200).json({
+        message: "Query executed successfully",
+        results: apartments
+    })
+}
+
 module.exports = {
-    getApartments
+    getApartments,
+    searchApartments
 }
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/apartments', apiControllers.getApartments);
+router.get('/apartments/search', apiControllers.searchApartments)
 
 module.exports = router;
