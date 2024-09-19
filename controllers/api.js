cc12d4877ece33dea079570e43cedeead9986783
omiras <oscar.miras@gmail.com>
2024-09-19 16:13:25 +0200

    limit apartments

--- controllers/api.js
+++ controllers/api.js
@@ -1,9 +1,18 @@
 const Apartment = require('../models/apartment.model');
 
 const getApartments = async (req, res) => {
-    const apartments = await Apartment.find();
+
+    const limit = req.query.limit || 100000;
+
+    if (limit < 1 || limit > 100000) {
+        return res.status(400).json({
+            message: "Limit parameter must be between 1 and 100000"
+        })
+    }
+
+    const apartments = await Apartment.find().limit(limit);
     return res.status(200).json({
-        message: "Operation success",
+        message: "Query executed successfully",
         results: apartments
     })
 }
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,6 @@ const apiControllers = require('../controllers/api');
 const express = require('express');
 const router = express.Router();
 
-router.get('/apartments', apiControllers.getApartments)
+router.get('/apartments', apiControllers.getApartments);
 
 module.exports = router;
